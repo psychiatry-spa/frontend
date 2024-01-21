eb2279a155a59f6b429046fc74f81fdafdb90ba6
tboronenko <tboronenko@tboronenkos-MacBook-Air.local>
2024-01-21 13:16:38 +0200

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
-import React, { useRef } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
-import { Editor as TinyMCEEditor } from 'tinymce';
+import TextEditor from "./components/Editor";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-    const editorRef = useRef<TinyMCEEditor | null>(null);
-    const log = () => {
-     if (editorRef.current) {
-       console.log(editorRef.current.getContent());
-     }
-   };
-    return (
-        <>
-       <Editor
-         onInit={(evt, editor) => editorRef.current = editor}
-         initialValue="<p>This is the initial content of the editor.</p>"
-         init={{
-           height: 500,
-           menubar: false,
-           plugins: [
-             'advlist autolink lists link image charmap print preview anchor',
-             'searchreplace visualblocks code fullscreen',
-             'insertdatetime media table paste code help wordcount'
-           ],
-           toolbar: 'undo redo | formatselect | ' +
-           'bold italic backcolor | alignleft aligncenter ' +
-           'alignright alignjustify | bullist numlist outdent indent | ' +
-           'removeformat | help',
-           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-         }}
-       />
-       <button onClick={log}>Log editor content</button>
-     </>
-    )
+  return (
+    <Routes>
+      <Route path="/editor" element={<TextEditor />} />
+    </Routes>
+  );
 }
 
 export default App;
