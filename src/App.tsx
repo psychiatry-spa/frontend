270dc6ef8fdf02bbd58592a6492ede9c19f8c4ef
PavelsDenisovs <pavelsdenisovs1@gmail.com>
2024-02-07 21:24:54 +0200

    Delete CreatePost.tsx/PavelsDenisovs/23-fe-formating-data-on-the-cline-side

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/forms/LoginForm";
 import RegisterForm from "./components/forms/RegisterForm";
-import CreatePost from "./components/CreatePost";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/posts" element={<CreatePost />} />
       </Routes>
     </Router>
   );
--- src/components/CreatePost.tsx
+++
@@ -1,9 +0,0 @@
-
-
-const CreatePost = () => {
-  return (
-    <div>erojpi</div>
-  )
-}
-
-export default CreatePost
