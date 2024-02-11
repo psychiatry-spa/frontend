62347317494bd864835b5db872d42bfa2ec6ab84
PavelsDenisovs <pavelsdenisovs1@gmail.com>
2024-02-11 08:06:35 +0200

    NavBar/Pavel/35-fe-navbar

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { API_ENDPOINTS } from "../../constants/const";
 import useUsers from "../../hooks/useUsers";
 import Icon from "../icons/Icon";
-import NavBar from "../../layouts/NavBar";
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({
@@ -22,62 +21,49 @@ const LoginForm = () => {
   const handleSubmit = useUsers(API_ENDPOINTS.signIn, formData, "POST");
 
   return (
-    <>
-      <NavBar />
-      <div>
-        <form
-          className="p-4 w-80 m-auto border b-1px mt-5"
-          onSubmit={handleSubmit}
-        >
-          <h1 className="block text-center text-3xl pb-5 mt-5 font-bold">
-            Login
-          </h1>
-          <div className="mx-auto w-64 block">
-            <label className="block mb-2">Email</label>
-            <input
-              className="border-b p-1 pl-2 w-64 h-8 outline-none"
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Your email"
-            />
-          </div>
-          <div className="mx-auto w-64 block">
-            <label className="block mt-5 mb-2">Password </label>
-            <input
-              className="border-b pl-2 p-1 w-64  h-8 outline-none"
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Your password"
-            />
-          </div>
-          <button
-            className="my-4 mx-auto w-32 h-8 rounded bg-blue-400 font-bold text-white block"
-            type="submit"
-          >
-            Submit
-          </button>
-          <div className="text-3xl text-center">or</div>
-          <Link
-            className="mx-auto block w-64 my-4"
-            to={API_ENDPOINTS.googleLogin}
-          >
-            <div className="w-64 border inline-flex items-center p-1">
-              <Icon name="google" />
-              <p className="pl-4 text-xl">Login with google</p>
-            </div>
-          </Link>
-          <button className="block mx-auto mt-5">
-            <Link className="text-gray-400" to="/signup">
-              Don't have an account
-            </Link>
-          </button>
-        </form>
+    <form className="p-4 w-80 m-auto border b-1px mt-5" onSubmit={handleSubmit}>
+      <h1 className="block text-center text-3xl pb-5 mt-5 font-bold">Login</h1>
+      <div className="mx-auto w-64 block">
+        <label className="block mb-2">Email</label>
+        <input
+          className="border-b p-1 pl-2 w-64 h-8 outline-none"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Your email"
+        />
       </div>
-    </>
+      <div className="mx-auto w-64 block">
+        <label className="block mt-5 mb-2">Password </label>
+        <input
+          className="border-b pl-2 p-1 w-64  h-8 outline-none"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="Your password"
+        />
+      </div>
+      <button
+        className="my-4 mx-auto w-32 h-8 rounded bg-blue-400 font-bold text-white block"
+        type="submit"
+      >
+        Submit
+      </button>
+      <div className="text-3xl text-center">or</div>
+      <Link className="mx-auto block w-64 my-4" to={API_ENDPOINTS.googleLogin}>
+        <div className="w-64 border inline-flex items-center p-1">
+          <Icon name="google" />
+          <p className="pl-4 text-xl">Login with google</p>
+        </div>
+      </Link>
+      <button className="block mx-auto mt-5">
+        <Link className="text-gray-400" to="/signup">
+          Don't have an account
+        </Link>
+      </button>
+    </form>
   );
 };
 
