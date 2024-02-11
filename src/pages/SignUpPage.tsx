163145e2a1747f982275aa38862f4c1b22231524
PavelsDenisovs <pavelsdenisovs1@gmail.com>
2024-02-11 08:15:18 +0200

    Add routering to NavBar/Pavel/36-feadd-routering-to-navbar

---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import Layout from "../layouts/Layout";
+import RegisterForm from "../components/forms/RegisterForm";
+
+const SignUpPage = () => {
+  return (
+    <Layout>
+      <RegisterForm />
+    </Layout>
+  );
+};
+
+export default SignUpPage;
