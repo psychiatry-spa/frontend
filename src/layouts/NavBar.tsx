ecc689eac9137b3122bbe02ae328b51480d6fb82
Timurs Boronenko <b.timurs.b@gmail.com>
2024-02-21 19:49:06 +0200

    Implement dockerfile.

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div className="fixed top-0 left-0 bg-white z-50 w-full border-b-2">
       <ul className="flex">
         <li className="p-4 pr-32">
-          <div className="inline">LV</div>/<div className="inline">RU</div>/
+          <div className="inline">LOH</div>/<div className="inline">RU</div>/
           <div className="inline">EN</div>
         </li>
         <li className="p-4">
