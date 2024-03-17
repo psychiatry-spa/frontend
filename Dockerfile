dc07a1702a6ff3ff1e06da53e61f94d4534ad9b1
Timurs Boronenko <b.timurs.b@gmail.com>
2024-03-17 20:52:10 +0200

    Change dockerfile for deployment.

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,10 @@ RUN npm install
 
 COPY . .
 
+ARG NODE_ENV=production
+
+ENV NODE_ENV=${NODE_ENV}
+
 RUN npm run build
 
 EXPOSE 5173
--- src/constants/const.ts
+++ src/constants/const.ts
@@ -1,8 +1,9 @@
 const BASE_URL = "http://localhost:3000";
+const PROD_BASE_URL = "https://mi-project-backend-opa7fhbyqq-uc.a.run.app"
 export const API_ENDPOINTS = {
-  signUp: `${BASE_URL}/api/auth/signUp`,
-  signIn: `${BASE_URL}/api/auth/signIn`,
-  googleLogin: `${BASE_URL}/api/auth/provider/google`,
-  usersList: `${BASE_URL}/api/admin/users`,
-  currentUser: `${BASE_URL}/api/user/me`
+  signUp: `${process.env.NODE_ENV === "production" ? PROD_BASE_URL : BASE_URL}/api/auth/signUp`,
+  signIn: `${process.env.NODE_ENV === "production" ? PROD_BASE_URL : BASE_URL}/api/auth/signIn`,
+  googleLogin: `${process.env.NODE_ENV === "production" ? PROD_BASE_URL : BASE_URL}/api/auth/provider/google`,
+  usersList: `${process.env.NODE_ENV === "production" ? PROD_BASE_URL : BASE_URL}/api/admin/users`,
+  currentUser: `${process.env.NODE_ENV === "production" ? PROD_BASE_URL : BASE_URL}/api/user/me`
 };
