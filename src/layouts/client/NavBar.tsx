9a8a28299c1b43e6336c04b8d044d8ffb6946480
Pavel Denisov <pavelsdenisovs1@gmail.com>
2024-04-01 23:24:53 +0300

    Remade navbar for desktop

--- src/layouts/client/NavBar.tsx
+++ src/layouts/client/NavBar.tsx
@@ -2,36 +2,43 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className="fixed py-5 h-20 flex justify-between top-0 left-0 bg-white z-50 w-full border-b-2">
-      <img className="w-16 h-16" src="src\assets\images\monkey.png" />
-      <ul className="flex">
-        <li>
-          <Link className="p-5 rounded-xl font-semibold text-coral" to="/">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="p-5 rounded-xl font-semibold" to="/sessions">
-            Schedule
-          </Link>
-        </li>
-        <li>
-          <Link className="p-5 rounded-xl font-semibold" to="/about">
-            Doctor
-          </Link>
-        </li>
-        <li>
-          <Link className="p-5 rounded-xl font-semibold" to="/register">
-            News
-          </Link>
-        </li>
-      </ul>
-      <div>
-        <Link className="p-4 rounded-xl font-semibold" to="/login">
+    <nav className="fixed h-20 flex justify-between top-0 left-0 bg-white z-50 w-full border-b-2">
+      <img className="m-1 w-16 h-16" src="src\assets\images\monkey.png" />
+      <div className="flex">
+        <Link
+          className="flex items-center justify-center px-3 mx-1 hover:bg-gray-200 font-semibold text-coral"
+          to="/"
+        >
+          Home
+        </Link>
+        <Link
+          className="flex items-center justify-center px-3 mx-1 hover:bg-gray-200 font-semibold"
+          to="/sessions"
+        >
+          Schedule
+        </Link>
+        <Link
+          className="flex items-center justify-center px-3 mx-1 hover:bg-gray-200 font-semibold"
+          to="/about"
+        >
+          Doctor
+        </Link>
+        <Link
+          className="flex items-center justify-center px-3 mx-1  hover:bg-gray-200 font-semibold"
+          to="/register"
+        >
+          News
+        </Link>
+      </div>
+      <div className="flex">
+        <Link
+          className="flex items-center justify-center h-11 px-3 mx-1 my-auto hover:bg-gray-200 rounded-xl font-semibold"
+          to="/login"
+        >
           Log in
         </Link>
         <Link
-          className="p-4 rounded-xl text-white font-semibold bg-coral"
+          className="flex items-center justify-center h-11 px-3 mx-1 my-auto rounded-xl text-white font-semibold bg-coral"
           to="/register"
         >
           Register
