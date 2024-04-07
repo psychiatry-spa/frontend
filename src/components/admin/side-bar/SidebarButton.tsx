1f7a40322d2aa51112c0cb6171c0e6b30d5a6eb5
Pavel Denisov <pavelsdenisovs1@gmail.com>
2024-04-07 20:07:11 +0300

    pd/117 refactor/Optimize the definition of isActive and add Link instead of div

--- src/components/admin/side-bar/SidebarButton.tsx
+++ src/components/admin/side-bar/SidebarButton.tsx
@@ -1,27 +1,15 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Icon from "../../common/icon";
-import { useEffect } from "react";
 
 interface Props {
   name: string;
 }
 
-export const SidebarButton = ({ name }: Props) => {
-  const navigate = useNavigate();
-  const location = useLocation();
+export const SidebarButton = ({name}: Props) => {
   const isActive = location.pathname === `/admin/${name.toLowerCase()}`;
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    navigate(`/admin/${name.toLowerCase()}`);
-  };
-
-  useEffect(() => {
-    isActive && console.log(isActive);
-  }, [isActive]);
-
   return (
-    <button
+    <Link
       className={`w-56 py-2 mx-2 my-1 flex items-center rounded-lg
       hover:bg-primary-100 hover:text-primary-800 
       dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700
@@ -30,12 +18,12 @@ export const SidebarButton = ({ name }: Props) => {
           ? " bg-primary-100 text-primary-800 font-medium"
           : " text-primary-700"
       }`}
-      onClick={handleClick}
+      to={`/admin/${name.toLowerCase()}`}
     >
       <Icon name={name.toLowerCase()} styles="m-1 mr-3 size-5" />
       <span className="text-[1.1rem]">
         {name.replace(/^\w/, (c) => c.toUpperCase())}
       </span>
-    </button>
+    </Link>
   );
 };
