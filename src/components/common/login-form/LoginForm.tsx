1ee090eafe2028e9cd7970313b798e75130cdd55
Pavel Denisov <pavelsdenisovs1@gmail.com>
2024-04-28 00:23:41 +0300

    pd/121 Add register form validation

--- src/components/common/login-form/LoginForm.tsx
+++ src/components/common/login-form/LoginForm.tsx
@@ -7,6 +7,7 @@ import Socials from "../socials/Socials";
 import { Link, useNavigate } from "react-router-dom";
 import Icon from "../icon";
 import Container from "../Container";
+import { useValidation } from "../../../hooks/useValidation";
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({
@@ -24,28 +25,30 @@ const LoginForm = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const validation = await useValidation(formData);
+    if (validation && validation.errors.length > 0) {
+      validation.errors.forEach((error) => {
+        switch (error) {
+          case "Short password":
+            setPasswordCharactersError(true);
+            break;
+          case "Incorrect email or password":
+            setIncorrectError(true);
+            break;
+          case "Weak password":
+            setPasswordError(true);
+            break;
+          case "Invalid email format":
+            setEmailError(true);
+            break;
+        }
+      });
+      return 0;
+    };
     const result = await submitForm(formData);
-    if (result) {
-      if (!result.ok && result.errors) {
-        result.errors.forEach((error) => {
-          switch (error) {
-            case "Short password":
-              setPasswordCharactersError(true);
-              break;
-            case "Incorrect email or password":
-              setIncorrectError(true);
-              break;
-            case "Weak password":
-              setPasswordError(true);
-              break;
-            case "Invalid email format":
-              setEmailError(true);
-              break;
-          }
-        });
-      }
-      result.ok && navigate("/admin/dashboard");
-    }
+    if (!result.ok) {
+      setIncorrectError(true);
+    } else navigate("/admin/dashboard")
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -54,10 +57,10 @@ const LoginForm = () => {
       ...prevState,
       [name]: value,
     }));
-    passwordCharactersError && setPasswordCharactersError(false);
-    emailError && setEmailError(false);
-    passwordError && setPasswordError(false);
-    incorrectError && setIncorrectError(false);
+    setPasswordCharactersError(false);
+    setEmailError(false);
+    setPasswordError(false);
+    setIncorrectError(false);
   };
 
   const isDisabled = () => !formData.email || !formData.password;
@@ -80,7 +83,11 @@ const LoginForm = () => {
           </p>
         )}
         <InputField
-          styles={passwordError || incorrectError || passwordCharactersError ? "border-red-500" : ""}
+          styles={
+            passwordError || incorrectError || passwordCharactersError
+              ? "border-red-500"
+              : ""
+          }
           data={formData.password}
           type="password"
           handleChange={handleChange}
--- src/hooks/api/useSubmitForm.ts
+++ src/hooks/api/useSubmitForm.ts
@@ -1,57 +1,34 @@
 const useSubmitForm = (endpoint: string) => {
-  return async (formData?: { email: string; password: string }) => {
-    const errors: string[] = [];
+  return async (formData: {
+    fullname?: string;
+    email: string;
+    password: string;
+  }) => {
+    try {
+      const options: RequestInit = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: formData ? JSON.stringify(formData) : undefined,
+      };
 
-    if (formData)
-      try {
-        if (formData.password.length < 6) {
-          console.error("Password must be at least 6 characters long");
-          errors.push("Short password");
-        } else {
-          let passwordRegex =
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-          if (!passwordRegex.test(formData.password)) {
-            console.error(
-              "At least 1 uppercase, lowercase, number and special character."
-            );
-            errors.push("Weak password");
-          }
-        }
+      const response = await fetch(endpoint, options);
 
-        let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-        if (!emailRegex.test(formData.email)) {
-          console.error(
-            `Please enter a valid email address. Example: "name@domain.com."`
-          );
-          errors.push("Invalid email format");
-        }
-
-        if (errors.length > 0) return { ok: false, errors };
-
-        const options: RequestInit = {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-          body: formData ? JSON.stringify(formData) : undefined,
-        };
-
-        const response = await fetch(endpoint, options);
-
-        if (!response.ok) {
-          console.error("Form submission failed");
-          errors.push("Incorrect email or password");
-          return { ok: false, errors };
-        }
-
-        const data = await response.json();
-        console.log(data);
-        return { ok: true, data };
-      } catch (error) {
-        console.error("Error while submitting the form:", error);
-        return { ok: false, error };
+      if (!response.ok) {
+        console.error("Form submission failed");
+        const error = "Incorrect email or password";
+        return { ok: false, error: error };
       }
+
+      const data = await response.json();
+      console.log(data);
+      return { ok: true, data };
+    } catch (error) {
+      console.error("Error while submitting the form:", error);
+      return { ok: false, error };
+    }
   };
 };
 
