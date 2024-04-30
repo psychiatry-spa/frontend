3d677c964a1fcd4b1f52551300da37fdf3d287a5
Timurs Boronenko <b.timurs.b@gmail.com>
2024-04-30 10:53:38 +0300

    Fixed body background.

--- src/layouts/client/ClientLayout.tsx
+++ src/layouts/client/ClientLayout.tsx
@@ -9,8 +9,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <>
       <NavBar />
-      {/* TODO: h-full or h-dvh  */}
-      <div className="flex justify-center items-center h-full bg-red-500">
+      <div className="flex justify-center items-center h-full bg-primary-005">
         <div className="mt-20">
           {children}
         </div>
