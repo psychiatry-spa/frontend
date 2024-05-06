afdcfe4b7f19ce93043a7bbe0e450b09ce015b0d
Pavel Denisov <pavelsdenisovs1@gmail.com>
2024-05-06 21:07:48 +0300

    Refactor hooks a little

--- src/hooks/api/useSubmitForm.ts
+++ src/hooks/api/useSubmitForm.ts
@@ -17,16 +17,12 @@ const useSubmitForm = (endpoint: string) => {
       const response = await fetch(endpoint, options);
 
       if (!response.ok) {
-        console.error("Form submission failed");
         const error = "Incorrect email or password";
         return { ok: false, error: error };
       }
-
       const data = await response.json();
-      console.log(data);
       return { ok: true, data };
     } catch (error) {
-      console.error("Error while submitting the form:", error);
       return { ok: false, error };
     }
   };
